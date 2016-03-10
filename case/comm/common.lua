35054253cee88a2f2e90756441cb40b6267509cd
lizhangyong <lizhangyong2008@163.com>
2016-03-10 21:12:44 +0800

    公共信息模块

--- case/comm/common.lua
+++ case/comm/common.lua
@@ -3,5 +3,5 @@ module("lua.comm.common", package.seeall)
 -- to prevent use of casual module global variables
 getmetatable(lua.comm.common).__newindex = function (table, key, val)
     error('attempt to write to undeclared variable "' .. key .. '": '
-            .. debug.traceback())
+            .. debug.traceback()) 
 end
