a50f4a7ab5eba2c75f6eb9c10c9da8b3b432c587
李张勇 <lizhangyong2008@163.com>
2016-03-11 18:31:30 +0800

    更新主程序

--- case/lua/access_check.lua
+++ case/lua/access_check.lua
@@ -1,66 +1,19 @@
-local ngx_req = ngx.req.get_uri_args
+
+local mid  = ngx.req.get_uri_args().mid
 
 local function check_mid(mid)
-    if mid ~= nil then
-        --local m = ngx.re.match(mid, "[0-9a-fA-F]+", "o")
-        local m = string.match(mid, "^[a-zA-Z0-9]+$")
-        if m and string.len(mid) <= 32 then
-            return true
-        else
-            return false
-        end
-    else
-       return false
-    end
-end
-
-local function check_ip(ip)
-    if ip == nil then
+    if mid == nil then
         return false
+    end 
+    
+    local m = ngx.re.match(mid, "^[0-9a-zA-Z]+$", "o")
+    if m and string.len(mid) <= 32 then
+        return true
     end
-    ---通配符^在gmatch中怎么使用？
-    if string.find(ip, "^%d") then
-    --if ngx.re.find(ip, "^%d", "o") then
-        for i,j,k,w in string.gmatch(ip, "(%d+)%.(%d+)%.(%d+)%.(%d+)") do
-        --for i,j,k,w in ngx.re.gmatch(ip, "(%d+)%.(%d+)%.(%d+)%.(%d+)", "o") do
-            if tonumber(i) >0 and tonumber(i) < 256 and tonumber(j) > 0 and tonumber(j) < 256
-                   and tonumber(k) > 0 and tonumber(k) < 256 and tonumber(w) > 0 and tonumber(w) < 256 then
-                return true
-            end
-        end
-    end
-    return false 
-end
-
-local mid = nil
-local ip = nil
-local args = ngx_req()
-for key, val in pairs(args) do
-    if key == "mid" then
-        if type(val) == "string" and val ~= nil then
-            mid = val
-        else
-            break
-        end    
-    elseif key == "ip" then
-        if type(val) == "string" and val ~= nil then
-            ip = val
-        else
-            break
-        end
-    else
-        break
-    end
+    return false
 end
 
 if not check_mid(mid) then
-    ngx.say("invalid mid")
+    ngx.log(ngx.ERR, "invalid mid: ", mid)
     ngx.exit(ngx.HTTP_BAD_REQUEST)
-    return
-elseif not check_ip(ip) then
-    ngx.say("invalid ip")
-    ngx.exit(ngx.HTTP_BAD_REQUEST)
-    return
-else
---    ngx.say("access_check_ok")
 end
--- case/lua/resty_case.lua
+++ case/lua/resty_case.lua
@@ -9,18 +9,15 @@ local mid = ngx.req.get_uri_args().mid
 --从缓存中查找mid对应的ip
 local res, err = cache_op.get_cache(mid)  --todo: 不存在于缓存时的返回值res？
 if not res or err then
-    ngx.log(ngx.ERR, err)
-    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-end
-
---命中缓存
-if res ~= ngx.null then
-   return ngx.say(“cache hit: “..cached_ip)
+    ngx.log(ngx.ERR, "cache not hit for mid: ", mid)
+else
+   --命中缓存
+   return ngx.say("result is: ", res)
 end
 
 --从数据库中查找mid对应的ip
 local tb_name = config.MYSQL_TABLE  --todo： 要测试下配置文件中不存在的情况
-local cmd = ....                    --todo: 考虑sql注入
+local cmd = string.format("select ip from %s where mid = \'%s\'", tb_name, mid)                   --todo: 考虑sql注入
 local res, err = db_op.do_cmd(cmd)
 if not res or err then
     ngx.log(ngx.ERR, err)
@@ -28,15 +25,14 @@ if not res or err then
 end
 
 --数据库中有结果，更新缓存
-if type(res) == "table" and res[1]["ip"] ~= nil then
+if type(res) == "table" and res[1] ~= nil and res[1]["ip"] ~= nil then
      cache_op.set_cache(mid, res[1]["ip"])
-     return ngx.say(“cache hit: “..res[1]["ip"])
+     return ngx.say("result is: ", res[1]["ip"])
 else
      --如果都没找到，更新客户端的ip到mysql和redis
-    local cmd = ""  --更新数据库操作
+    local cmd = string.format("insert into %s(mid, ip) values(\'%s\', \'%s\')", tb_name, mid, cli_ip)  --todo:考虑sql注入
     db_op.do_cmd(cmd)
     cache_op.set_cache(mid, cli_ip)
-    ngx.log(ngx.WARN, "cache not hit!")
-    return ngx.say(“cache not hit!“)
+    ngx.log(ngx.WARN, "no result found!")
+    return ngx.say("no result found!")
 end
-
