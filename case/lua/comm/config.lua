43160b26dea32dbed60137b48e26337dd1af0a9a
李张勇 <lizhangyong2008@163.com>
2016-03-11 18:32:43 +0800

    添加config.lua

---
+++ case/lua/comm/config.lua
@@ -0,0 +1,8 @@
+module("lua.comm.config", package.seeall)
+
+MYSQL_TABLE = 'resty_case'
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = '3306'
+MYSQL_DB = 'resty_test'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = '123456'
