368e83042fc7e8b2301016a7d5f622ab96368158
李张勇 <lizhangyong2008@163.com>
2016-03-11 18:29:10 +0800

    mysql建表语句更新

--- case/tools/mysql_init.lua
+++ case/tools/mysql_init.lua
@@ -1,1 +1,9 @@
 
+CREATE TABLE `resty_test`.`resty_case` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`mid` varchar(32) NOT NULL,
+	`ip` varchar(32) NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `mid_index` USING HASH (`mid`) comment ''
+) COMMENT='resty test table';
+
