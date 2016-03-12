72a34a418c0aabc92e5032e520a95a9a63960c2f
lizhangyong <lizhangyong2008@163.com>
2016-03-13 00:05:51 +0800

    更新模块定义方式

--- case/lua/first_cache/first_cache.lua
+++ case/lua/first_cache/first_cache.lua
@@ -1,7 +1,6 @@
-module(..., package.seeall)
+module(..., package.seeall)  --todo 最好修改成local _M = {} 的方式
 
 local cache  = require "lua.first_cache.db_redis"
-
 
 function conn_cache()
     return cache:conn_cache()
@@ -15,3 +14,20 @@ function set_cache(key, value)
     return cache:set_cache(key, value)
 end
 
+--以下代码暂未测试，先提交
+--local _M = {}
+--local cache  = require "lua.first_cache.db_redis"
+
+--function _M.conn_cache(self)
+--    return cache:conn_cache()
+--end
+
+--function _M.get_cache(self, key)
+--    return cache:get_cache(key)
+--end
+
+--function _M.set_cache(self, key, value)
+--    return cache:set_cache(self, key, value)
+--end
+
+--return _M
