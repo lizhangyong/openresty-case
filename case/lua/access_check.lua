475f8f05b7ad60728c65a24f7cb1aec34aefe1c2
lizhangyong <lizhangyong2008@163.com>
2016-03-12 23:58:51 +0800

    去掉主控程序中的函数
    暂未测试，先注释。

--- case/lua/access_check.lua
+++ case/lua/access_check.lua
@@ -17,3 +17,15 @@ if not check_mid(mid) then
     ngx.log(ngx.ERR, "invalid mid: ", mid)
     ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
+
+--local mid = ngx.var.arg_mid
+--if mid == nil then
+--    ngx.log(ngx.ERR, "invalid mid: nil")
+--    ngx.exit(ngx.HTTP_BAD_REQUEST)
+--else
+--    local m = ngx.re.match(mid, "^[0-9a-zA-Z]+$", "o")
+--    if not m or string.len(mid) > 32 then
+--        ngx.log(ngx.ERR, "invalid mid: ", mid)
+--        ngx.exit(ngx.HTTP_BAD_REQUEST)
+--    end
+--end
