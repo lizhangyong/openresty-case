03709d667613421ee10896c2e74a2867defd2b68
lizhangyong <lizhangyong2008@163.com>
2016-03-13 00:08:12 +0800

    更新模块定义方式

--- case/lua/db/db.lua
+++ case/lua/db/db.lua
@@ -12,4 +12,19 @@ function do_cmd(cmd)
     end
 end
 
-return _M
+--以下代码暂未测试， 先注释
+--local db  = require "lua.db.db_mysql"
+
+--local _M = {}
+
+--function _M.conn_db(self)
+--    return db:conn_db()
+--end
+
+--function _M.do_cmd(self, cmd)
+--    if cmd then
+--        return db:do_cmd(cmd)
+--    end
+--end
+
+--return _M
