4c8d153c87453ccf898015f5b919d72399ef2190
lizhangyong <lizhangyong2008@163.com>
2016-03-12 23:46:33 +0800

    考虑sql注入情况

--- case/lua/resty_case.lua
+++ case/lua/resty_case.lua
@@ -5,19 +5,27 @@ local db_op = require "lua.db.db"
 
 local cli_ip = ngx.var.remote_addr
 local mid = ngx.req.get_uri_args().mid
+--local mid = ngx.var.arg_mid
 
 --从缓存中查找mid对应的ip
-local res, err = cache_op.get_cache(mid)  --todo: 不存在于缓存时的返回值res？
+local res, err = cache_op.get_cache(mid)
 if not res or err then
     ngx.log(ngx.ERR, "cache not hit for mid: ", mid)
+    --ngx.log(ngx.ERR, "cache not hit for mid: ", mid, ' err:', err)
 else
    --命中缓存
    return ngx.say("result is: ", res)
 end
 
 --从数据库中查找mid对应的ip
-local tb_name = config.MYSQL_TABLE  --todo： 要测试下配置文件中不存在的情况
-local cmd = string.format("select ip from %s where mid = \'%s\'", tb_name, mid)                   --todo: 考虑sql注入
+local tb_name = "resty_case"  --set default 
+if config.MYSQL_TABLE then
+    tb_name = config.MYSQL_TABLE
+end
+                 
+--local quote_mid = ngx.quote_sql_str(mid)            --todo: 考虑sql注入
+--local cmd = [[select ip from ]] .. tb_name .. [[ where mid=]] .. quote_mid .. [[ limit 1;]] 
+local cmd = string.format("select ip from %s where mid = \'%s\'", tb_name, mid)
 local res, err = db_op.do_cmd(cmd)
 if not res or err then
     ngx.log(ngx.ERR, err)
@@ -29,8 +37,10 @@ if type(res) == "table" and res[1] ~= nil and res[1]["ip"] ~= nil then
      cache_op.set_cache(mid, res[1]["ip"])
      return ngx.say("result is: ", res[1]["ip"])
 else
-     --如果都没找到，更新客户端的ip到mysql和redis
-    local cmd = string.format("insert into %s(mid, ip) values(\'%s\', \'%s\')", tb_name, mid, cli_ip)  --todo:考虑sql注入
+     --如果都没找到，更新客户端的ip到mysql和redis 
+    --todo:考虑sql注入
+    --local cmd = string.format("insert into %s(mid, ip) values(%s, \'%s\')", tb_name, mid, cli_ip)  
+    local cmd = string.format("insert into %s(mid, ip) values(\'%s\', \'%s\')", tb_name, mid, cli_ip)  
     db_op.do_cmd(cmd)
     cache_op.set_cache(mid, cli_ip)
     ngx.log(ngx.WARN, "no result found!")
