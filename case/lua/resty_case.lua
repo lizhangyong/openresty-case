692db7390361a9761ae20181e5d809854bd675a2
李张勇 <lizhangyong2008@163.com>
2016-03-15 12:34:57 +0800

    更新主程序中其他模块的调用方式，以及sql语句

--- case/lua/resty_case.lua
+++ case/lua/resty_case.lua
@@ -4,14 +4,13 @@ local cache_op   = require "lua.first_cache.first_cache"
 local db_op = require "lua.db.db"
 
 local cli_ip = ngx.var.remote_addr
-local mid = ngx.req.get_uri_args().mid
---local mid = ngx.var.arg_mid
+--local mid = ngx.req.get_uri_args().mid
+local mid = ngx.var.arg_mid
 
 --从缓存中查找mid对应的ip
-local res, err = cache_op.get_cache(mid)
+local res, err = cache_op:get_cache(mid)
 if not res or err then
-    ngx.log(ngx.ERR, "cache not hit for mid: ", mid)
-    --ngx.log(ngx.ERR, "cache not hit for mid: ", mid, ' err:', err)
+    ngx.log(ngx.ERR, "cache not hit for mid: ", mid, ' err:', err)
 else
    --命中缓存
    return ngx.say("result is: ", res)
@@ -23,10 +22,10 @@ if config.MYSQL_TABLE then
     tb_name = config.MYSQL_TABLE
 end
                  
---local quote_mid = ngx.quote_sql_str(mid)            --todo: 考虑sql注入
+local quote_mid = ngx.quote_sql_str(mid) 
 --local cmd = [[select ip from ]] .. tb_name .. [[ where mid=]] .. quote_mid .. [[ limit 1;]] 
-local cmd = string.format("select ip from %s where mid = \'%s\'", tb_name, mid)
-local res, err = db_op.do_cmd(cmd)
+local cmd = string.format("select ip from %s where mid = %s limit 1", tb_name, quote_mid)  
+local res, err = db_op:do_cmd(cmd)
 if not res or err then
     ngx.log(ngx.ERR, err)
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
@@ -34,15 +33,13 @@ end
 
 --数据库中有结果，更新缓存
 if type(res) == "table" and res[1] ~= nil and res[1]["ip"] ~= nil then
-     cache_op.set_cache(mid, res[1]["ip"])
+     cache_op:set_cache(quote_mid, res[1]["ip"])
      return ngx.say("result is: ", res[1]["ip"])
 else
      --如果都没找到，更新客户端的ip到mysql和redis 
-    --todo:考虑sql注入
-    --local cmd = string.format("insert into %s(mid, ip) values(%s, \'%s\')", tb_name, mid, cli_ip)  
-    local cmd = string.format("insert into %s(mid, ip) values(\'%s\', \'%s\')", tb_name, mid, cli_ip)  
-    db_op.do_cmd(cmd)
-    cache_op.set_cache(mid, cli_ip)
+    local cmd = string.format("insert into %s(mid, ip) values(%s, \'%s\')", tb_name, quote_mid, cli_ip)  
+    db_op:do_cmd(cmd)
+    cache_op:set_cache(quote_mid, cli_ip)
     ngx.log(ngx.WARN, "no result found!")
     return ngx.say("no result found!")
 end
