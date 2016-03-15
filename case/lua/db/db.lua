bd3be04b989bd35346f0ef6b90709fd7d08e29a7
李张勇 <lizhangyong2008@163.com>
2016-03-15 12:36:01 +0800

    修改模块的定义方式为_M方式

--- case/lua/db/db.lua
+++ case/lua/db/db.lua
@@ -1,30 +1,16 @@
-module(..., package.seeall)
 
 local db  = require "lua.db.db_mysql"
 
-function conn_db()
+local _M = {}
+
+function _M.conn_db(self)
     return db:conn_db()
 end
 
-function do_cmd(cmd)
+function _M.do_cmd(self, cmd)
     if cmd then
         return db:do_cmd(cmd)
     end
 end
 
---以下代码暂未测试， 先注释
---local db  = require "lua.db.db_mysql"
-
---local _M = {}
-
---function _M.conn_db(self)
---    return db:conn_db()
---end
-
---function _M.do_cmd(self, cmd)
---    if cmd then
---        return db:do_cmd(cmd)
---    end
---end
-
---return _M
+return _M
--- case/lua/first_cache/first_cache.lua
+++ case/lua/first_cache/first_cache.lua
@@ -1,33 +1,18 @@
-module(..., package.seeall)  --todo 最好修改成local _M = {} 的方式
 
 local cache  = require "lua.first_cache.db_redis"
 
-function conn_cache()
+local _M = {}
+
+function _M.conn_cache(self)
     return cache:conn_cache()
 end
 
-function get_cache(key)
+function _M.get_cache(self, key)
     return cache:get_cache(key)
 end
 
-function set_cache(key, value)
+function _M.set_cache(self, key, value)
     return cache:set_cache(key, value)
 end
 
---以下代码暂未测试，先提交
---local _M = {}
---local cache  = require "lua.first_cache.db_redis"
-
---function _M.conn_cache(self)
---    return cache:conn_cache()
---end
-
---function _M.get_cache(self, key)
---    return cache:get_cache(key)
---end
-
---function _M.set_cache(self, key, value)
---    return cache:set_cache(self, key, value)
---end
-
---return _M
+return _M
