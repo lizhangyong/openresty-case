c77c06b10f9a0b7b5a10d24250fff88f33a3dab9
李张勇 <lizhangyong2008@163.com>
2016-03-15 12:26:11 +0800

    Revert "去掉主控流程中的函数"
    This reverts commit 3e63ced4491b11a75454ecaa1ff5b6bb25331422.

--- case/lua/access_check.lua
+++ case/lua/access_check.lua
@@ -1,13 +1,31 @@
 
+local mid  = ngx.req.get_uri_args().mid
 
-local mid = ngx.var.arg_mid
-if mid == nil then
-    ngx.log(ngx.ERR, "invalid mid: nil")
-    ngx.exit(ngx.HTTP_BAD_REQUEST)
-else
+local function check_mid(mid)
+    if mid == nil then
+        return false
+    end 
+    
     local m = ngx.re.match(mid, "^[0-9a-zA-Z]+$", "o")
-    if not m or string.len(mid) > 32 then
-        ngx.log(ngx.ERR, "invalid mid: ", mid)
-        ngx.exit(ngx.HTTP_BAD_REQUEST)
+    if m and string.len(mid) <= 32 then
+        return true
     end
+    return false
 end
+
+if not check_mid(mid) then
+    ngx.log(ngx.ERR, "invalid mid: ", mid)
+    ngx.exit(ngx.HTTP_BAD_REQUEST)
+end
+
+--local mid = ngx.var.arg_mid
+--if mid == nil then
+--    ngx.log(ngx.ERR, "invalid mid: nil")
+--    ngx.exit(ngx.HTTP_BAD_REQUEST)
+--else
+--    local m = ngx.re.match(mid, "^[0-9a-zA-Z]+$", "o")
+--    if not m or string.len(mid) > 32 then
+--        ngx.log(ngx.ERR, "invalid mid: ", mid)
+--        ngx.exit(ngx.HTTP_BAD_REQUEST)
+--    end
+--end
