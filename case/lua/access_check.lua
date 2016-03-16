bda7c0b6f8ffca63eea7e4ea863738d41c0e7c35
李张勇 <lizhangyong2008@163.com>
2016-03-16 19:19:41 +0800

    修改正则表达式

--- case/lua/access_check.lua
+++ case/lua/access_check.lua
@@ -5,9 +5,10 @@ if mid == nil then
     ngx.log(ngx.ERR, "invalid mid: nil")
     ngx.exit(ngx.HTTP_BAD_REQUEST)
 else
-    local m = ngx.re.match(mid, "^[0-9a-zA-Z]+$", "o")
-    if not m or string.len(mid) > 32 then
+    local m = ngx.re.match(mid, "^[0-9a-zA-Z]{0,32}$", "o")
+    if not m then
         ngx.log(ngx.ERR, "invalid mid: ", mid)
         ngx.exit(ngx.HTTP_BAD_REQUEST)
     end
 end
+
