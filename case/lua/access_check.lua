3fa6dea36ccacb492785bda97d164d5057798d2e
李张勇 <lizhangyong2008@163.com>
2016-03-21 16:27:18 +0800

    更新程序

--- case/lua/access_check.lua
+++ case/lua/access_check.lua
@@ -1,19 +1,7 @@
 
-
 local mid = ngx.var.arg_mid
-if mid == nil then
-    ngx.log(ngx.ERR, "invalid mid: nil")
+if mid == nil or not ngx.re.match(mid, "^[0-9a-zA-Z]{32}$", "o") then
+    ngx.log(ngx.INFO, "invalid mid: ", mid)
     ngx.exit(ngx.HTTP_BAD_REQUEST)
-else
-    local m = ngx.re.match(mid, "^[0-9a-zA-Z]{0,32}$", "o")
-    if not m then
-        ngx.log(ngx.ERR, "invalid mid: ", mid)
-        ngx.exit(ngx.HTTP_BAD_REQUEST)
-    end
 end
-
---if mid == nil or not ngx.re.match(mid, "^[0-9a-zA-Z]{0,32}$", "o") then
---    ngx.log(ngx.INFO, "invalid mid: ", mid)
---    ngx.exit(ngx.HTTP_BAD_REQUEST)
---end
 
